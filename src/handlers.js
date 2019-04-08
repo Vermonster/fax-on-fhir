cc057dec805da65ef99af9f3de6d368dd9de38c7
Chris Wand <chris@vermonster.com>
2019-04-08 16:09:36 -0400

    WIP

--- src/handlers.js
+++ src/handlers.js
@@ -1,39 +1,95 @@
-const AWS = require('aws-sdk');
-const multipart = require('parse-multipart');
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
+const request = require('request');
+const OCR = require('tesseract.js');
+const fs = require('fs');
+const tar = require('tar');
+const imageMagick = require('gm').subClass({imageMagick: true});
 
-exports.pdfUploadHandler = async (request, context, callback) => {
-  console.log('Version 3:06')
-  console.log(request.body);
-  const file = await parseMultiPartFormData(request);
-  console.log(file);
-  await uploadToS3(file);
-  //const text = ocrPDF(pdf);
-  //await createComprehendJob(text, id);
+exports.pdfUploadHandler = async (event, context, callback) => {
+  console.log('Version 10:08')
+  downloadFax(event);
 
-  return {statusCode: 200, body: 'Repeat'}
+  callback(null, "Success")
 }
 
 exports.comprehendCompletionHandler = async (event, context, callback) => {
-  const classification = await getComprehendResults(event);
-  const pdf = await getPDF(event);
-  const binaryResponse = await uploadFHIRBinary(pdf);
-  await uploadFHIRDocumentReference(classification, binaryResponse);
+  const s3Event = event.Records[0].s3;
+  if (event.Records[0].s3.object.key.match('write_access_check_file.temp')) {
+    return { statusCode: 200 }
+  };
+  const faxId = s3Event.object.key.match(/(.+)\/.+\/output/)[1];
+  s3.getObject({
+    Bucket: s3Event.bucket.name,
+    Key: s3Event.object.key
+  }, (err, data) => {
+    fs.writeFileSync('/tmp/output.tar.gz', data.Body);
+    tar.extract({ file: '/tmp/output.tar.gz', cwd: '/tmp/', sync: true });
+    const output = fs.readFileSync('/tmp/predictions.jsonl')
+    const docType = output.Classes.reduce((prev, curr) => {
+      return (prev.Score > curr.Score) ? prev : curr
+    }).Name
+
+    s3.getObject({
+      Bucket: process.env.S3_BUCKET_FOR_FAX,
+      Key: faxId + '.tif'
+    })
+
+  });
+  callback(null, "Success");
+  //const classification = await getComprehendResults(event);
+  //const pdf = await getFax(event);
+  //const binaryResponse = await uploadFHIRBinary(pdf);
+  //await uploadFHIRDocumentReference(classification, binaryResponse);
 }
 
-const parseMultiPartFormData = (request) => {
-  const boundary = multipart.getBoundary(request.headers['Content-Type']);
+const downloadFax = async (event) => {
+  const bodyAttrs = {};
 
-  const bodyBuffer = Buffer.from(request.body, 'utf-8');
-  return multipart.Parse(bodyBuffer, boundary).find((field) => field.name === "Media");
+  event.body.split('&').forEach((attrStr) => {
+    let attrPair = attrStr.split('=');
+    bodyAttrs[attrPair[0]] = decodeURIComponent(attrPair[1])
+  })
+
+  const pdfUrl = bodyAttrs.MediaUrl;
+
+  request.get({ uri: pdfUrl, encoding: null }, async (err, resp, body) => {
+    const filename = bodyAttrs.FaxSid + '.tif';
+    uploadToS3(body, filename);
+
+    fs.writeFileSync('/tmp/' + filename, body);
+    imageMagick('/tmp/' + filename).write('/tmp/temp.jpg', (err) => {
+      if(err) { console.log(err) };
+      OCR.recognize('/tmp/temp.jpg')
+      .then((result) => {
+        uploadToS3(result.text, bodyAttrs.FaxSid + '.txt', (err) => {
+          if(err) { console.log(err) };
+          const comprehend = new AWS.Comprehend();
+          console.log(comprehend);
+          comprehend.startDocumentClassificationJob({
+            DataAccessRoleArn: process.env.CLASSIFIER_ROLE_ARN,
+            DocumentClassifierArn: process.env.CLASSIFIER_ARN,
+            InputDataConfig: {
+              S3Uri: `s3://${process.env.S3_BUCKET_FOR_FAX}/${bodyAttrs.FaxSid}.txt`,
+              InputFormat: 'ONE_DOC_PER_FILE'
+            },
+            OutputDataConfig: {
+              S3Uri: `s3://${process.env.S3_BUCKET_FOR_COMPREHEND_RESULTS}/${bodyAttrs.FaxSid}`
+            }
+          }, (err, data) => console.log(err, data));
+        });
+      });
+    });
+
+    return body;
+  })
 }
 
-const uploadToS3 = async (file) => {
-  const s3 = new AWS.S3();
-
-  console.log(file.data.toString());
-  await s3.putObject({
-    Bucket: process.env.S3_BUCKET_FOR_PDF,
-    Key: file.filename,
-    Body: file.data
-  }).promise((err, data) => console.log(err, data))
+const uploadToS3 = async (file, filename, callback = (err, data) => console.log(err, data)) => {
+  console.log(`Uploading ${filename}`);
+  s3.putObject({
+    Bucket: process.env.S3_BUCKET_FOR_FAX,
+    Key: filename,
+    Body: file
+  }, callback);
 }
